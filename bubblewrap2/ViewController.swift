f9730ebf2f1f85cc4003752dcf77968a084eb63c
John Cui <jcui@john-cuis-mac-mini.local>
2016-02-04 17:54:37 -0600

    k

--- bubblewrap2/ViewController.swift
+++ bubblewrap2/ViewController.swift
@@ -11,12 +11,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let stackView = UIStackView()
 
-    let button1 = UIButton(type: UIButtonType.System)
-    let image1 = UIImage(named: "ghost.png") as UIImage?
-    let image2 = UIImage(named: "pumpkin.png") as UIImage?
-    
+    let screenSize: CGRect = UIScreen.mainScreen().bounds
+    let image1 = UIImage(named: "bubb.png") as UIImage?
+    let image2 = UIImage(named: "bubb2.png") as UIImage?
+    var buttonArr: [UIButton] = [UIButton]()
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -25,16 +24,23 @@ class ViewController: UIViewController {
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
-        button1.setImage(image2, forState: .Normal)
-        view.addSubview(stackView)
-        stackView.addArrangedSubview(button1)
-        
-        let deepPressGestureRecognizer = DeepPressGestureRecognizer(target: self, action: "deepPressHandler:", threshold: 0.75)
-        
-        button1.addGestureRecognizer(deepPressGestureRecognizer)
-        
-        stackView.addArrangedSubview(button1)
+        var count = 0
+        let screenWidth = Int(screenSize.width)
+        let screenHeight = Int(screenSize.height)
+        for var i = 0; i < screenWidth; i += 50 {
+            for var j = 30; j < screenHeight-30; j += 50 {
+                var button1 = UIButton(frame: CGRectMake(CGFloat(i),CGFloat(j), 55, 55))
+                button1.tag = count
+                count = count + 1
+                button1.setImage(image2, forState: .Normal)
+                let deepPressGestureRecognizer = DeepPressGestureRecognizer(target: self, action: "deepPressHandler:", threshold: 0.75)
+                
+                button1.addGestureRecognizer(deepPressGestureRecognizer)
+                buttonArr.append(button1)
+                self.view.addSubview(button1)
+                
+            }
+        }
         
        
         
@@ -46,7 +52,18 @@ class ViewController: UIViewController {
         {
             print("deep press begin: ", value.view?.description)
             UIDevice.currentDevice()._tapticEngine().actuateFeedback(1001)
-            button1.setImage(image1, forState: .Normal)
+            if let button = value.view as? UIButton {
+                // use button
+                var num = button.tag
+                buttonArr[num].setImage(image1, forState: .Normal)
+                buttonArr[num].enabled = false
+                print(button.tag)
+                
+            }
+            var touchLocation: CGPoint = value.locationInView(value.view)
+            print(touchLocation)
+            //touchLocation = self.convertPointFromView(touchLocation)
+            //button1.setImage(image1, forState: .Normal)
             
         }
         else if value.state == UIGestureRecognizerState.Ended
@@ -55,7 +72,7 @@ class ViewController: UIViewController {
         }
     }
     
-  
+  /*
     override func viewDidLayoutSubviews()
     {
         stackView.axis = UILayoutConstraintAxis.Vertical
@@ -67,6 +84,7 @@ class ViewController: UIViewController {
             width: view.frame.width,
             height: view.frame.height - topLayoutGuide.length).insetBy(dx: 50, dy: 100)
     }
+*/
 }
 
 class DeepPressableButton: UIButton, DeepPressable
